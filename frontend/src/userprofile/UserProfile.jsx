26aabe71e6db2f509f8713f141a844f18b0ca2e0
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-08-30 14:11:54 -0400

    refer to previous commit

---
+++ frontend/src/userprofile/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserProfile({ userprofiles }) {
+  return (
+    <div className="user-profile">
+        <h2>Profile</h2>
+    </div>
+  );
+}
+
+export default UserProfile;
