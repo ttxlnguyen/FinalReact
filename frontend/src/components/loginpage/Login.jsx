e09c86475d08e89d65279ff22b27be4b3f151f5b
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-09-02 13:47:24 -0400

    refactored Login.js and split into .js & .jsx

---
+++ frontend/src/components/loginpage/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useLogin } from './Login';
+
+function Login({ onLoginSuccess }) {
+  const { username, setUsername, password, setPassword, error, handleSubmit } = useLogin(onLoginSuccess);
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
