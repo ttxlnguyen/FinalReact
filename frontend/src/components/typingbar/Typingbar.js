bf6b4c730560a88651bd55e301537af7bbcdcb7d
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-09-04 08:46:03 -0400

    latest

--- frontend/src/components/typingbar/Typingbar.js
+++ frontend/src/components/typingbar/Typingbar.js
@@ -5,13 +5,13 @@ import './Typingbar.css';
 // This component renders the input field for typing messages and the send button
 function Typingbar({ inputMessage, handleInputChange, handleMessageSubmit }) {
   return (
-    <div className='typing-bar'>
+    <div className="typing-bar">
       <form onSubmit={handleMessageSubmit} className="typing-bar">
         <input
           type="text"
           value={inputMessage}
           onChange={handleInputChange}
-          placeholder="Type your message here..."
+          placeholder="  Type your message here..."
         />
         <button type="submit">Send</button>
       </form>
