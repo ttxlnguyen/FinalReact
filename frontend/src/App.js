c3b5afc5873a5278671baf2761bc100ec43aa81f
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-09-03 21:52:47 -0400

    separated everything related to messages-list in channels.css and placed into new messages.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar.js';
 import Channels from './components/channels/Channels.js';
-import Messages from './Messages/Messages';
+import Messages from './components/messages/Messages.js';
 import MessageList from './components/MessageList';
 import MessageInput from './components/typingbar/Typingbar.js';
 import Login from './components/loginpage/Login.jsx';
