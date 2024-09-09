54ac3e4c55557e114b766df5e93100ce4d0743f0
igordon215 <igordon215@gmail.com>
2024-09-09 15:59:15 -0400

    commented out code in App.js for sidebar toggle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -64,8 +64,8 @@ function App() {
   // Handle channel selection
   const handleChannelSelect = (channelId) => {
     selectChannel(channelId);
-    // Removed: setIsChannelListOpen(false);
-    setIsMessagesOpen(true);
+    // UN-comment to restor sidebar colapse: setIsChannelListOpen(false);
+    // UN-comment to restor sidebar colapse: setIsMessagesOpen(true);
     if (isLoggedIn) {
       fetchMessages(channelId);
     }
